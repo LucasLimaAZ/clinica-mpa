ad460c711e02791e76264e556b63a15f7893ed78
Lucas de Lima Monteiro <lucasdelimamonteiro@gmail.com>
2024-09-27 22:12:46 -0300

    base url for production

--- src/shared/services/http-client.ts
+++ src/shared/services/http-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: "https://eternia.com.br/sistemamepa/api/",
   headers: {
     "Content-Type": "application/json",
   },
